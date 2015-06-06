cda00cd05eefeea91f0258e3bf6b2f3214d9e62a
Giovanni Pinto <jop.mezzana@gmail.com>
2015-06-06 12:13:05 +0200

    Initialized goodreads apis

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var music   = require('./music-spotify');
 var movies  = require('./movies-rottentomatoes');
+var books   = require('./books-goodreads');
 var app = express();
 
 app.set('port', (process.env.PORT || 3666));
@@ -12,6 +13,7 @@ app.use(function(req, res, next) {
 
 app.use('/music', music);
 app.use('/movies', movies);
+app.use('/books', books);
 
 app.listen(app.get('port'), function() {
   console.log("App is running at localhost:" + app.get('port'));
--- books-goodreads.js
+++ books-goodreads.js
@@ -1,8 +1,32 @@
 var express       = require('express');
 var SpotifyWebApi = require('spotify-web-api-node');
 var EventEmitter  = require('events').EventEmitter;
+var goodreads     = require('goodreads'); 
 
 var eventEmitter = new EventEmitter();
 var router = express.Router();
 
-var apiKey = require('./goodReadsApiKey');
+var apiKeys;
+if (process.env.SPOTIFY_CLIENTID){
+  apiKeys = {};
+  apiKeys.key = process.env.GOODREADS_KEY;
+  apiKeys.secret = process.env.GOODREADS_SECRET;
+}
+else{
+  apiKeys = require('./goodreadsApiKey');
+}
+
+var goodreadsApi = new goodreads.client(apiKeys);
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
