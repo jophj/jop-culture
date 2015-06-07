da10acc8a4d0dbe73e31127188038684bc0d4c12
Giovanni Pinto <jop.mezzana@gmail.com>
2015-06-07 13:35:53 +0200

    Steam api key is stored correctly stored and loaded

--- games-steam.js
+++ games-steam.js
@@ -1,12 +1,29 @@
+var express       = require('express');
+var SteamApi      = require('steam-api');
+var EventEmitter  = require('events').EventEmitter;
+
+var eventEmitter = new EventEmitter();
+var router = express.Router();
+
+
+
+var steamApiKey;
+if (process.env.STEAM_APIKEY){
+  steamApiKey = process.env.STEAM_APIKEY;
+}
+else{
+  steamApiKey = require('./steamApiKey');
+}
+
+var player = new SteamApi.Player(steamApiKey);
+
+
 var callback = function(result){
     console.log('steam test');
     console.log(result);
   };
   
 console.log('steam test');
-var SteamApi = require('steam-api');
-var player = new SteamApi.Player(
-  '12CF2FB52A9BFD9E8493335CCB30B54F');
 console.log(player);
 player.GetOwnedGames('76561198017403191', true, true, []).done(
   callback
